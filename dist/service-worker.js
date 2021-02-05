45e44200c7e93ca3ec7a733c4416c1e6e2b6ae33
qasem hajizadeh <gasemhacker@gmail.com>
2021-02-06 03:24:38 +0330

    rebuild strofront:

---
+++ dist/service-worker.js
@@ -0,0 +1,32 @@
+importScripts("/precache-manifest.e7838bde22adb56ed2c64cdaf04fbbcd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front",
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg|csv|pdf)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [
+      new RegExp("/graphql"),
+      new RegExp("/dashboard"),
+      new RegExp("/media/export_files"),
+      new RegExp("/plugins"),
+      new RegExp("/storybook"),
+      new RegExp("/__"), // used by cypress tests runner
+    ],
+  }
+);
+
