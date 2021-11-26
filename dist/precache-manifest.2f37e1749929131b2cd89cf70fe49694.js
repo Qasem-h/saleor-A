e9d6a0695989d3690a05c1c673e8276f31867562
qasem hajizadeh <gasemhacker@gmail.com>
2021-11-26 15:42:58 +0330

    new build

---
+++ dist/precache-manifest.2f37e1749929131b2cd89cf70fe49694.js
@@ -0,0 +1,389 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/app.f0c36836e75259991130.css"
+  },
+  {
+    "revision": "0a89f5545f8c9e316f586cee1c5172f4",
+    "url": "/images/ReadymadeCurtainsAndBlinds.png"
+  },
+  {
+    "revision": "3e7059edb733c322e5a6032f6657f5ec",
+    "url": "/images/ServiceAndMaintainance.jpg"
+  },
+  {
+    "revision": "3ded73a37ffec8562d3d84cca8729c6e",
+    "url": "/images/Tents.jpg"
+  },
+  {
+    "revision": "11b18380b19cd001121daf87b7959d7d",
+    "url": "/images/address.svg"
+  },
+  {
+    "revision": "9922c0f69aeb8e9820a583f784453762",
+    "url": "/images/amex.svg"
+  },
+  {
+    "revision": "8dd427951293264bf209bbbef698176f",
+    "url": "/images/angle-up.png"
+  },
+  {
+    "revision": "2edbf568491472c1f48ed97cd1abfacf",
+    "url": "/images/arrow-back.svg"
+  },
+  {
+    "revision": "24d174f4a5a1a34940852d713a66cec0",
+    "url": "/images/arrow.svg"
+  },
+  {
+    "revision": "59bbf99c247abcc1c1a557e61455f5cb",
+    "url": "/images/breadcrumb-bg.jpg"
+  },
+  {
+    "revision": "2ad0ba8414993cff1309b758211536db",
+    "url": "/images/breadcrumbs-arrow.svg"
+  },
+  {
+    "revision": "46e5dfb2cdad1754ee16cd521e551b7a",
+    "url": "/images/carousel-arrow.svg"
+  },
+  {
+    "revision": "fcabe0268ad362c8b4681594d3411e4c",
+    "url": "/images/carpetsAndRugs.png"
+  },
+  {
+    "revision": "93666d457ded3a4cbea59c8efbfe635d",
+    "url": "/images/cart.svg"
+  },
+  {
+    "revision": "55eb7c054a23791ef6a1dcc97f888e04",
+    "url": "/images/curtains1.jpg"
+  },
+  {
+    "revision": "423a626c9c5659e6f50e9fc0fb8fff18",
+    "url": "/images/discover.svg"
+  },
+  {
+    "revision": "2e4067cd0aa49a52dcf10ccb26ff9fe5",
+    "url": "/images/email.svg"
+  },
+  {
+    "revision": "2b887a8b9ba4dc6e059a5dca1c91d427",
+    "url": "/images/facebook-icon.svg"
+  },
+  {
+    "revision": "b3361a65b1eddda83705dbeed96a283f",
+    "url": "/images/favicons/android-chrome-144x144.png"
+  },
+  {
+    "revision": "2cd2aff91db66d9327d1452bfb84106f",
+    "url": "/images/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "08383e70706c58ccff35d55382ad0005",
+    "url": "/images/favicons/android-chrome-256x256.png"
+  },
+  {
+    "revision": "e19ebe863ab546d13ee363d7a45ebb09",
+    "url": "/images/favicons/android-chrome-36x36.png"
+  },
+  {
+    "revision": "b266f8922396c26cb7ea7f703d9eca74",
+    "url": "/images/favicons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "3603e288390fae80a76e3eae28dcc0b5",
+    "url": "/images/favicons/android-chrome-48x48.png"
+  },
+  {
+    "revision": "c486409a698400185bc4f07c6e19f2bd",
+    "url": "/images/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "589ea1c1b26841504825c27d4f5fc19e",
+    "url": "/images/favicons/android-chrome-72x72.png"
+  },
+  {
+    "revision": "7239e1aa266c0cc68cbf6d14f8eb83fe",
+    "url": "/images/favicons/android-chrome-96x96.png"
+  },
+  {
+    "revision": "fd8a304cb071df95c585116890d752b7",
+    "url": "/images/favicons/apple-touch-icon-1024x1024.png"
+  },
+  {
+    "revision": "451411e49757b6c6361c62232fb5cfce",
+    "url": "/images/favicons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "03a6d7ccd3c33e887fc8a485682e78e1",
+    "url": "/images/favicons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "4870b951aea7696f64a02a441318d356",
+    "url": "/images/favicons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "01d0b0cd86aeaafcf36452ca3a2b773e",
+    "url": "/images/favicons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "946ac375e41368108db9cb8f87da9d9d",
+    "url": "/images/favicons/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "5c75f11430cae0b3626a3e993398c590",
+    "url": "/images/favicons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "f2b818d43ed50ab013cbdb9468c235d1",
+    "url": "/images/favicons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "e5b0a110124fa9ce98dc711aa583fbdb",
+    "url": "/images/favicons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "72b4c84c3c6b5fb6522817b819efe490",
+    "url": "/images/favicons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "ca7470316e66d6d3eb324f34f1b41ff0",
+    "url": "/images/favicons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "5c75f11430cae0b3626a3e993398c590",
+    "url": "/images/favicons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "5c75f11430cae0b3626a3e993398c590",
+    "url": "/images/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "9bd9c68ef2f1cc2099d98b63415fab26",
+    "url": "/images/favicons/apple-touch-startup-image-1182x2208.png"
+  },
+  {
+    "revision": "7da4b5ce7bd388a0f9c2347b483a954a",
+    "url": "/images/favicons/apple-touch-startup-image-1242x2148.png"
+  },
+  {
+    "revision": "40c92c8de1571de0c67234b0d543f0da",
+    "url": "/images/favicons/apple-touch-startup-image-1496x2048.png"
+  },
+  {
+    "revision": "664fb1788a1743b5f7501fc9682d38ca",
+    "url": "/images/favicons/apple-touch-startup-image-1536x2008.png"
+  },
+  {
+    "revision": "65e7b353321ebea4b719bc03e99cec84",
+    "url": "/images/favicons/apple-touch-startup-image-320x460.png"
+  },
+  {
+    "revision": "5d77699dc552b62c3b69b6f2a4184657",
+    "url": "/images/favicons/apple-touch-startup-image-640x1096.png"
+  },
+  {
+    "revision": "7e924e928cf2e5cc9f125164d78a7a36",
+    "url": "/images/favicons/apple-touch-startup-image-640x920.png"
+  },
+  {
+    "revision": "ff2e76cf745910d36b5f9933f7c0e0b4",
+    "url": "/images/favicons/apple-touch-startup-image-748x1024.png"
+  },
+  {
+    "revision": "ab392b4506a95960dcc7ed4afd623a52",
+    "url": "/images/favicons/apple-touch-startup-image-750x1294.png"
+  },
+  {
+    "revision": "ac67e1324e8e75035916af39c54c7c50",
+    "url": "/images/favicons/apple-touch-startup-image-768x1004.png"
+  },
+  {
+    "revision": "a4c4ae44d03d70fbddcb8a75c47dedd4",
+    "url": "/images/favicons/browserconfig.xml"
+  },
+  {
+    "revision": "e6ceabdae52d437ec8b49f6bad6e594a",
+    "url": "/images/favicons/coast-228x228.png"
+  },
+  {
+    "revision": "38481ccba1cb1c9f813041e7ecfb1ba2",
+    "url": "/images/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "677ad5feb0aedf8dd890bae9f22b21ac",
+    "url": "/images/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "e4f027b5da67307190f5d152dd0615e0",
+    "url": "/images/favicons/favicon.ico"
+  },
+  {
+    "revision": "1f651fc2ac103df532a6b393ba9e657e",
+    "url": "/images/favicons/firefox_app_128x128.png"
+  },
+  {
+    "revision": "e162a1ac34c8b47a2861982b89dcfe30",
+    "url": "/images/favicons/firefox_app_512x512.png"
+  },
+  {
+    "revision": "f1570047ba5f27ee35b65d7993cc1844",
+    "url": "/images/favicons/firefox_app_60x60.png"
+  },
+  {
+    "revision": "aa4b9efdb21f604a9e4e5f34f8a9c102",
+    "url": "/images/favicons/manifest.json"
+  },
+  {
+    "revision": "e4d6cd87627bb245f6db0bd57a205189",
+    "url": "/images/favicons/manifest.webapp"
+  },
+  {
+    "revision": "b3361a65b1eddda83705dbeed96a283f",
+    "url": "/images/favicons/mstile-144x144.png"
+  },
+  {
+    "revision": "85d474e4d8fb29aee471d2351fe26730",
+    "url": "/images/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "0ef20bca7226c25db81476e334414f0d",
+    "url": "/images/favicons/mstile-310x150.png"
+  },
+  {
+    "revision": "a8673ca3a99910c66e2e00c2a7e04bb1",
+    "url": "/images/favicons/mstile-310x310.png"
+  },
+  {
+    "revision": "86c15d37374ca1e844feb2417db14d48",
+    "url": "/images/favicons/mstile-70x70.png"
+  },
+  {
+    "revision": "7c2abcb9e4492e0bc6eaeeccb7d4df7c",
+    "url": "/images/favicons/yandex-browser-50x50.png"
+  },
+  {
+    "revision": "74ca5bb0ff10841d62d3bfa09e6b41bc",
+    "url": "/images/favicons/yandex-browser-manifest.json"
+  },
+  {
+    "revision": "5c4afd2b27a91e5537318acfdaa75ccf",
+    "url": "/images/furnitureUpholstery.jpg"
+  },
+  {
+    "revision": "72f0b80556965d101b99735a5382c1b5",
+    "url": "/images/garbage.svg"
+  },
+  {
+    "revision": "60ba99ba49ebcb48acf3455ee22ef22b",
+    "url": "/images/hamburger-hover.svg"
+  },
+  {
+    "revision": "5bacf6d70b4037a57f01cf8393e0b4d9",
+    "url": "/images/hamburger.svg"
+  },
+  {
+    "revision": "fcf36f17cd46ff2e4c2f03a729429bd4",
+    "url": "/images/instagram-icon.svg"
+  },
+  {
+    "revision": "a55a99602b17aaa4585c15dc7c490aa6",
+    "url": "/images/interiorDesign.jpg"
+  },
+  {
+    "revision": "35555a736d713af1d68af3f399b116f8",
+    "url": "/images/jcb.svg"
+  },
+  {
+    "revision": "f554f27d42d11cb90968fdbff39fca1b",
+    "url": "/images/logo-small.svg"
+  },
+  {
+    "revision": "4036356da3da4a71cebc361997c1050d",
+    "url": "/images/logo.svg"
+  },
+  {
+    "revision": "9d17cdd532fb79575cc394a6aa698008",
+    "url": "/images/maestro.svg"
+  },
+  {
+    "revision": "50b1bd2483c8925bd359272d376440a8",
+    "url": "/images/mastercard.svg"
+  },
+  {
+    "revision": "114a0c361476162e7ab92a80efcf4038",
+    "url": "/images/no-photo.svg"
+  },
+  {
+    "revision": "6550b1a860a20f0ca15c03c0f8aef409",
+    "url": "/images/open_hour.svg"
+  },
+  {
+    "revision": "d2de6fc9d698453e09f6195c6cc08fb4",
+    "url": "/images/phone.svg"
+  },
+  {
+    "revision": "794c5f9fc0af2947d89b369df1cf96b2",
+    "url": "/images/quote-icon.svg"
+  },
+  {
+    "revision": "07215f861aee3c59fc915786e3835fd0",
+    "url": "/images/search.svg"
+  },
+  {
+    "revision": "a7f238d4d559f842518ba2df6d768cda",
+    "url": "/images/skill-bg.jpg"
+  },
+  {
+    "revision": "1bcc0613c469c683d0a6f7a6a42b28cb",
+    "url": "/images/slider-bg-1.jpg"
+  },
+  {
+    "revision": "f7e80b2793aa832268cdbd57e3ed6650",
+    "url": "/images/slider-bg-2.jpg"
+  },
+  {
+    "revision": "a328d54fc3009a727c8d176fe415d801",
+    "url": "/images/slider-bg-3.jpg"
+  },
+  {
+    "revision": "58ad01863fb9a6ec83c0d010ac297629",
+    "url": "/images/slider-bg-4.jpg"
+  },
+  {
+    "revision": "2cbed1f3f96e36f656d71fd4d20b4322",
+    "url": "/images/subcategories.svg"
+  },
+  {
+    "revision": "37ecefda00ea508da1af9967961d8053",
+    "url": "/images/twitter-icon.svg"
+  },
+  {
+    "revision": "5a93a28a584dc3157f803e6176b7181d",
+    "url": "/images/user.svg"
+  },
+  {
+    "revision": "eb63482e3c6153359a36eb5abc604bde",
+    "url": "/images/visa.svg"
+  },
+  {
+    "revision": "0e1c56253c410756b66c0f0bea95bb18",
+    "url": "/images/wallpaper.jpg"
+  },
+  {
+    "revision": "e5ef826e86e2ce50fc42e6fe21737602",
+    "url": "/images/x.svg"
+  },
+  {
+    "revision": "bad6a4dac178fc5763986a5eb42b644b",
+    "url": "/images/youtube-icon.svg"
+  },
+  {
+    "revision": "6597eebe0281f669c33646e1d2aa7d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e32d51a62af172ca639",
+    "url": "/js/app.b1922a5365e7ed53d192.js"
+  }
+]);
